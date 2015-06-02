6f8f9a012e21030cf463c9c802bec686b50be0d9
Eric Nordmeyer <en@v2a.net>
2015-06-02 09:14:18 +0200

    Allow empty password, when editing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 4 }
+  validates :password, presence:true, length: { minimum: 4 }, allow_nil: true
   has_secure_password
 
   def User.digest(string)
