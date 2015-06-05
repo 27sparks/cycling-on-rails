32e97beace2516c183744507867ce7cb6c6de81f
Eric Nordmeyer <en@v2a.net>
2015-06-05 10:41:12 +0200

    add activities to users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @activities = @user.activities
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
