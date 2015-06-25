fa491ba8899e1481ec21a442593f2211f80bc6ae
Eric Nordmeyer <en@v2a.net>
2015-06-25 14:28:34 +0200

    define ranges and refactor statistics js

--- app/assets/javascripts/statistics.js
+++ app/assets/javascripts/statistics.js
@@ -1,25 +1,48 @@
 $(document).ready(function() {
     var time_frame = 'year'
     var date = ($.urlParam('date') == null ? new Date().toJSON().slice(0,10) : $.urlParam('date'))
+    var minDate = new Date();
+    minDate.setMonth(0);
+    minDate.setDate(1);
+    var maxDate = new Date();
+    maxDate.setMonth(11);
+    maxDate.setDate(31);
     var options = {
+
         chart: {
             renderTo: 'dashboard-chart',
             zoomType: 'x'
         },
         title: { text: 'Dashboard' },
-        yAxis: [{ title: { text: '' }},
-                { title: { text: '' },
-                    opposite: true
+        yAxis: [{ title: { text: '' },
+                    min: 0
                 },
-                { title: { text: '' }}
+                { title: { text: '' },
+                    min: 0
+                },
+                { title: { text: '' },
+                    min: 0,
+                    opposite: true
+                }
         ],
-        xAxis: { type: 'datetime' },
-        credits: { enabled: false }
+        xAxis: {
+            type: 'datetime',
+            min: new Date().setMonth(new Date().getMonth() - 3),
+            max: new Date().setMonth(new Date().getMonth() + 1)
+        },
+        credits: { enabled: false },
+        plotOptions: {
+            spline: {
+                connectNulls: true
+            }
+        }
     };
 
     function get_and_show_graph_from_json(url, index, array) {
         $.getJSON(url, function (data) {
+            data.yAxis = index;
             chart.addSeries(data);
+            if (data.unit == 'no_unit'){ data.unit = data.name }
             chart.yAxis[index].setTitle({text: data.unit})
         });
     }
@@ -50,7 +73,41 @@ $(document).ready(function() {
     show_tlf()
     $('#tlf').click(function(){show_tlf()});
     $('#add').click(function(){show_add()});
-    $('#year').click(function(){set_time_frame('year')});
-    $('#month').click(function(){set_time_frame('month')});
+    $('#standard_range').click(function(){
+        var min = new Date().setMonth(new Date().getMonth() - 3);
+        var max = new Date().setMonth(new Date().getMonth() + 1)
+        chart.xAxis[0].setExtremes(min,max);
+    });
+    $('#this_year').click(function(){
+        var min = new Date(new Date().getFullYear(), 0, 1);
+        var max = new Date(new Date().getFullYear(), 11, 31);
+        chart.xAxis[0].setExtremes(min,max);
+    });
+    $('#last_year').click(function(){
+        var min = new Date((new Date().getFullYear() - 1), 0, 1);
+        var max = new Date((new Date().getFullYear() - 1), 11, 31);
+        chart.xAxis[0].setExtremes(min,max);
+    });
+    $('#this_month').click(function(){
+        var min = new Date();
+        min.setDate(1);
+        var max = new Date();
+        max.setMonth(max.getMonth()+1)
+        max.setDate(1);
+        chart.xAxis[0].setExtremes(min,max);
+    });
+    $('#last_month').click(function(){
+        var min = new Date();
+        min.setDate(1);
+        min.setMonth(min.getMonth()-1);
+        var max = new Date();
+        max.setDate(1);
+        chart.xAxis[0].setExtremes(min,max);
+    });
+    $('#last_three_months').click(function(){
+        var x = new Date();
+        x.setMonth(x.getMonth()-3);
+        chart.xAxis[0].setExtremes(x,Date.now());
+    });
 
 });
