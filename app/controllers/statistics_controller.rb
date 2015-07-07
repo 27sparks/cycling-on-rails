4c418a5e797ae64d008464385640ff6e3e8a1dd1
Eric Nordmeyer <en@v2a.net>
2015-07-07 17:14:33 +0200

    refactor statistics_controller

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,6 +1,8 @@
 class StatisticsController < ApplicationController
   include StatisticsHelper
-  before_action :set_statistic, only: [:show, :edit, :update, :destroy]
+
+  LOAD_PERCENTAGE_ARRAY = [1,5,10,15,30,40,43,45,47,49,50,49,48,46,44,42,40,37,35,34,32,31,29,28,24,20,17,14,12,10,9,8,7,6,5,4,3,2,1]
+  FATIGUE_PERCENTAGE_ARRAY = [100,80,50,40,30,20,10,5,3,2,1]
 
   def index
     function_call = get_function_call params[:values], params[:unit]
@@ -11,53 +13,23 @@ class StatisticsController < ApplicationController
     case params[:values]
       when 'fatigue'
         activities.each do |activity|
-          case params[:time_frame]
-            when 'month'
-              (1..100).each do |i|
-                if @results[:data][activity.start_time.to_date.day + i - 1].nil? == false
-                  @results[:data][activity.start_time.to_date.day + i - 1] += ((activity.trimp / 50 / i) * 30.0).to_f
-                end
-              end
-            else
-              (1..100).each do |i|
-                if @results[:data][activity.start_time.to_date.yday + i- 1].nil? == false
-                  @results[:data][activity.start_time.to_date.yday + i - 1] += ((activity.trimp / 50 / i) * 20.0).to_f
-                end
-              end
+          day = params[:time_frame] == 'month' ? activity.start_time.to_date.day : activity.start_time.to_date.yday
+          FATIGUE_PERCENTAGE_ARRAY.each_with_index do |percent, i|
+            if @results[:data][day + i].nil? == false
+              @results[:data][day + i] += (activity.trimp / 100 * percent).to_f
+            end
           end
-
         end
       when 'load'
         activities.each do |activity|
-          case params[:time_frame]
-            when 'month'
-              (1..6).each do |i|
-                @results[:data][activity.start_time.to_date.day + i] += i if @results[:data][activity.start_time.to_date.day + i].present?
-              end
-              (7..100).each do |i|
-                @results[:data][activity.start_time.to_date.day + i] += 6.0 if @results[:data][activity.start_time.to_date.day + i].present?
-              end
-              (2..6).each do |i|
-                @results[:data][activity.start_time.to_date.day + i] -= ((activity.trimp / 200 / i) * 10.0).to_f if @results[:data][activity.start_time.to_date.day + i].present?
-              end
-              (7..100).each do |i|
-                @results[:data][activity.start_time.to_date.day + i] -= (i.to_f / 16) if @results[:data][activity.start_time.to_date.day + i].present?
-              end
-            else
-              (1..6).each do |i|
-                @results[:data][activity.start_time.to_date.yday + i] += i if @results[:data][activity.start_time.to_date.yday + i].present?
-              end
-              (7..100).each do |i|
-                @results[:data][activity.start_time.to_date.yday + i] += 6.0 if @results[:data][activity.start_time.to_date.yday + i].present?
-              end
-              (2..6).each do |i|
-                @results[:data][activity.start_time.to_date.yday + i] -= ((activity.trimp / 200 / i) * 10.0).to_f if @results[:data][activity.start_time.to_date.yday + i].present?
-              end
-              (7..100).each do |i|
-                @results[:data][activity.start_time.to_date.yday + i] -= (i.to_f / 16) if @results[:data][activity.start_time.to_date.yday + i].present?
-              end
+          day = params[:time_frame] == 'month' ? activity.start_time.to_date.day : activity.start_time.to_date.yday
+          LOAD_PERCENTAGE_ARRAY.each_with_index do |percent, i|
+            if @results[:data][day + i].nil? == false
+              @results[:data][day + i] += (activity.trimp / 100 * percent).to_f
+            end
           end
         end
+
       when 'avghr'
         activities.each do |activity|
           case
@@ -90,6 +62,8 @@ class StatisticsController < ApplicationController
     @results[:type] = case params[:unit]
                         when 'no_unit'
                           'line'
+                        when 'imp', 'bpm'
+                          'bar'
                         when 'load'
                           'areaspline'
                         else
@@ -102,55 +76,21 @@ class StatisticsController < ApplicationController
   # GET /statistics/1
   # GET /statistics/1.json
   def show
-  end
-
-  # GET /statistics/new
-  def new
-    @statistic = Statistic.new
-  end
-
-  # GET /statistics/1/edit
-  def edit
-  end
-
-  # POST /statistics
-  # POST /statistics.json
-  def create
-    @statistic = Statistic.new(statistic_params)
-
-    respond_to do |format|
-      if @statistic.save
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully created.' }
-        format.json { render :show, status: :created, location: @statistic }
-      else
-        format.html { render :new }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
+    activity = Activity.find(params[:id])
+    @result = {}
+    @result[:hrbpm] = []
+    @result[:alt] = []
+    @result[:distance] = []
+    activity.laps.each do |lap|
+      lap.track.track_points.each do |point|
+        @result[:hrbpm] << point.heart_rate_bpm
+        @result[:alt] << point.altitude_meters
+        @result[:distance] << point.distance_meters
       end
     end
-  end
-
-  # PATCH/PUT /statistics/1
-  # PATCH/PUT /statistics/1.json
-  def update
-    respond_to do |format|
-      if @statistic.update(statistic_params)
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully updated.' }
-        format.json { render :show, status: :ok, location: @statistic }
-      else
-        format.html { render :edit }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /statistics/1
-  # DELETE /statistics/1.json
-  def destroy
-    @statistic.destroy
-    respond_to do |format|
-      format.html { redirect_to statistics_url, notice: 'Statistic was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @result[:pointStart] = 0
+    @result[:pointInterval] = 1000
+    @result
   end
 
   private
